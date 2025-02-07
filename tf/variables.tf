88766393c57e0c701626ec370e901800ba22ed76
Tyrone Vriesde <tvriesde@microsoft.com>
2025-02-07 14:33:46 +0100

    update

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,4 +1,9 @@
-variable "GITHUB_TOKEN" {
+variable "github_token" {
   description = "GitHub Token to interact from Terraform"
+  type = string
+}
+
+variable "github_organization" {
+  description = "GitHub Organization to manage"
   type = string
 }
