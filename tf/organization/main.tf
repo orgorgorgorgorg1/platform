1c614b5cd36d92409cde28b9689fd8c2aa5f9ddc
Tyrone Vriesde <tvriesde@microsoft.com>
2025-03-12 11:19:09 +0100

    update

--- tf/organization/main.tf
+++ tf/organization/main.tf
@@ -6,7 +6,7 @@ provider "github" {
 resource "github_enterprise_organization" "red" {
   enterprise_id = data.github_enterprise.enterprise.id
   name          = "tyrone-org-red"
-  display_name  = "red"
+  display_name  = "tyrone-org-red"
   description   = "Organization created with terraform"
   billing_email = "tvriesde@gmail.com"
   admin_logins  = [
@@ -17,7 +17,7 @@ resource "github_enterprise_organization" "red" {
 resource "github_enterprise_organization" "green" {
   enterprise_id = data.github_enterprise.enterprise.id
   name          = "tyrone-org-green"
-  display_name  = "green"
+  display_name  = "tyrone-org-green"
   description   = "Organization created with terraform"
   billing_email = "tvriesde@gmail.com"
   admin_logins  = [
@@ -28,7 +28,7 @@ resource "github_enterprise_organization" "green" {
 resource "github_enterprise_organization" "sandbox" {
   enterprise_id = data.github_enterprise.enterprise.id
   name          = "tyrone-org-archive"
-  display_name  = "Some Awesome Org"
+  display_name  = "tyrone-org-archive"
   description   = "Organization created with terraform"
   billing_email = "tvriesde@gmail.com"
   admin_logins  = [
@@ -39,7 +39,7 @@ resource "github_enterprise_organization" "sandbox" {
 resource "github_enterprise_organization" "archive" {
   enterprise_id = data.github_enterprise.enterprise.id
   name          = "tyrone-org-sandbox"
-  display_name  = "Some Awesome Org"
+  display_name  = "tyrone-org-sandbox"
   description   = "Organization created with terraform"
   billing_email = "tvriesde@gmail.com"
   admin_logins  = [
