4d3069cc064c4c9c13064babdc235c45cdcdf444
Tyrone Vriesde <tvriesde@microsoft.com>
2025-03-13 13:27:25 +0100

    change archive

--- tf/organization/main.tf
+++ tf/organization/main.tf
@@ -23,8 +23,8 @@ resource "github_enterprise_organization" "green" {
 resource "github_enterprise_organization" "sandbox" {
   enterprise_id = data.github_enterprise.enterprise.id
   name          = "tyrone1-org-archive"
-  display_name  = "tyrone-org-archive"
-  description   = "Organization created with terraform"
+  display_name  = "tyrone99-org-archive"
+  description   = "Hoihoi"
   billing_email = "tvriesde@gmail.com"
   admin_logins  = [
     "tvriesde"
