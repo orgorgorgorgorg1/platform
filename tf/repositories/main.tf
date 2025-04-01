dad3de705d980c80e4f7b0d429b6568dfd7c1d98
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-01 15:39:04 +0200

    update

--- tf/repositories/main.tf
+++ tf/repositories/main.tf
@@ -11,14 +11,14 @@ locals {
 
 # Create a GitHub repository for each row in the CSV
 resource "github_repository" "repositories" {
-  for_each    = { for repo in local.repositories : repo.name => repo }
+  for_each    = { for repo in local.repositories : repo.Repository => repo }
   name        = each.value.name
   description = each.value.description
   private     = each.value.private
 }
 
 resource "github_team_repository" "team_permissions" {
-    for_each    = { for repo in local.repositories : repo.name => repo }
+    for_each    = { for repo in local.repositories : repo.Repository => repo }
     team_id    = repo.value.group
     repository = repo.value.name
     permission  = each.value.type == "project_admin" ? "admin" : "push"
