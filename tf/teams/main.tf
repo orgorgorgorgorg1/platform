06155b20254427c8583e53115efccf31efc90469
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-02 09:12:29 +0200

    update

---
+++ tf/teams/main.tf
@@ -0,0 +1,25 @@
+provider "github" { 
+  token = var.github_token
+  owner = var.github_organization
+}
+
+# Read and decode the CSV file
+locals {
+  teams_csv = file("${path.root}/csv/teams.csv")
+  repositories     = csvdecode(local.teams_csv)
+}
+
+# Create a GitHub repository for each row in the CSV
+resource "github_repository" "repositories" {
+  for_each    = { for repo in local.repositories : repo.id => repo }
+  name        = each.value.Repository
+  description = "placeholder"
+  visibility  = "private"
+}
+
+resource "github_team_repository" "team_permissions" {
+    for_each    = { for repo in local.repositories : repo.id => repo }
+    team_id    = each.value.Group
+    repository = each.value.Repository
+    permission  = each.value.Permission_Level == "PROJECT_ADMIN" ? "admin" : "push"
+ }
