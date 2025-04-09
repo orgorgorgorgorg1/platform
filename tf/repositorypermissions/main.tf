7a971d468280a99221bb07696e84e9b7c17f9e69
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-09 14:15:10 +0200

    update

--- tf/repositorypermissions/main.tf
+++ tf/repositorypermissions/main.tf
@@ -1,11 +1,24 @@
+provider "github" { 
+  token = var.github_token
+  owner = var.github_organization
+}
+
 # Read and decode the CSV file
 locals {
-  repositories_csv = file("${path.root}/csv/repositorypermissions.csv")
+  repositories_csv = file("${path.root}/csv/repos.csv")
   repositories     = csvdecode(local.repositories_csv)
 }
 
+# # Create a GitHub repository for each row in the CSV
+# resource "github_repository" "repositories" {
+#   for_each    = { for repo in local.repositories : repo.id => repo }
+#   name        = each.value.Repository
+#   description = "placeholder"
+#   visibility  = "private"
+# }
+
 resource "github_team_repository" "team_permissions" {
-    for_each    = { for permission in local.repositories : permission.id => permission }
+    for_each    = { for repo in local.repositories : repo.id => repo }
     team_id    = each.value.Group
     repository = each.value.Repository
     permission  = each.value.Permission_Level == "PROJECT_ADMIN" ? "admin" : "push"
--- tf/repositorypermissions/variables.tf
+++ tf/repositorypermissions/variables.tf
@@ -1,9 +1,9 @@
-# variable "github_token" {
-#   description = "GitHub Token to interact from Terraform"
-#   type = string
-# }
+variable "github_token" {
+  description = "GitHub Token to interact from Terraform"
+  type = string
+}
 
-# variable "github_organization" {
-#   description = "GitHub Organization to manage"
-#   type = string
-# }
+variable "github_organization" {
+  description = "GitHub Organization to manage"
+  type = string
+}
