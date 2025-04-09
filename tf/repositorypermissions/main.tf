b6138baeeabf8e02001c0f9d127981d0cb6563a7
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-09 15:48:22 +0200

    update

--- tf/repositorypermissions/main.tf
+++ tf/repositorypermissions/main.tf
@@ -1,11 +1,11 @@
 # Read and decode the CSV file
 locals {
-  repositories_csv = file("${path.root}/csv/repositorypermissions.csv")
-  repositories     = csvdecode(local.repositories_csv)
+  repositorypermissions_csv = file("${path.root}/csv/repositorypermissions.csv")
+  repositorypermissions     = csvdecode(local.repositorypermissions_csv)
 }
 
 resource "github_team_repository" "team_permissions" {
-    for_each    = { for repo in local.repositories : repo.id => repo }
+    for_each    = { for repo in local.repositorypermissions : repo.id => repo }
     team_id    = each.value.Group
     repository = each.value.Repository
     permission  = each.value.Permission_Level == "PROJECT_ADMIN" ? "admin" : "push"
