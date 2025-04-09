3517be7e217858752085af3763e83aa9463dc6ba
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-09 13:46:55 +0200

    add changes

--- tf/main.tf
+++ tf/main.tf
@@ -1,8 +1,8 @@
-module "organization" {
-  source = "./organization"
-  github_enterprise_slug = var.github_enterprise_slug
-  github_token = var.github_token
-}
+# module "organization" {
+#   source = "./organization"
+#   github_enterprise_slug = var.github_enterprise_slug
+#   github_token = var.github_token
+# }
 
 module "repositories" {
   source = "./repositories"
