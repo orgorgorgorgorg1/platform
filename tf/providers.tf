1812acd2b5072ad66b929e936ab169783d15a7df
Tyrone Vriesde <tvriesde@microsoft.com>
2025-04-15 16:50:53 +0200

    update

--- tf/providers.tf
+++ tf/providers.tf
@@ -20,6 +20,6 @@ provider "azurerm" {
 
 provider "github" { 
   token = var.github_token
-  owner = var.github_organization
+  organization = var.github_organization
   //for rotterdam, probably have to set base_url to the version that supports data locality
 }
